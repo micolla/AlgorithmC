6c85cba08980eee7658987c6e5f9ada2d88f4f24
Николай Донцов <nikolay.dontsov@yandex.ru>
2019-07-08 19:53:38 +0300

    all 3 exercise

--- Lesson2/Lesson2.cpp
+++ Lesson2/Lesson2.cpp
@@ -1,5 +1,50 @@
 #include <iostream>
 
+int RaiseSimple(int a, int b) {
+	if (b == 0)
+		return 1;
+	else {
+		int r = a;
+		for (int i = 1; i < b; i++) {
+			r *= a;
+		}
+		return r;
+	}
+}
+
+int RaiseRecursive(int a, int b) {
+	if (b > 0) {
+		return a * RaiseRecursive(a, b - 1);
+	}
+	else
+		return 1;
+}
+
+int RaiseRecursiveHalf(int a, int b) {
+	if (b > 0) {
+		if (b % 2 == 0) {
+			return RaiseRecursive(a * a, b / 2);
+		}
+		else
+			return a * RaiseRecursiveHalf(a, b-1);
+	}
+	else
+		return 1;
+}
+
+int convert(int x, int iter) {
+	if (x == 1) {
+		return 1* RaiseSimple(10,iter);
+	}
+	else {
+		return convert(x / 2, iter+1)+ (x % 2 != 0 ? 1 : 0 )* RaiseSimple(10, iter) ;
+	}
+}
+
+int DecToBin(int x) {
+	return convert(x, 0);
+}
+
 int MultiTwo(int x)
 {
 	return x * 2;
@@ -16,7 +61,7 @@ int R(int start, int end) {
 		i = 1;
 	}
 	else if (start < end) {
-		for (int j = 0;j < 2;j++) {
+		for (int j = 0; j < 2; j++) {
 			if (j == 0)
 				i += R(MultiTwo(start), end);
 			else
@@ -26,7 +71,18 @@ int R(int start, int end) {
 	return i;
 }
 
-void main()
+int main()
 {
-	printf("%d",R(2,22));
+    std::cout << DecToBin(8)<<"\n";
+	std::cout << DecToBin(9) << "\n";
+	std::cout << DecToBin(129) << "\n";
+	std::cout << RaiseRecursive(2, 2) << "\n";
+	std::cout << RaiseRecursive(2, 0) << "\n";
+	std::cout << RaiseRecursive(3, 3) << "\n";
+
+	std::cout << RaiseRecursiveHalf(2, 8) << "\n";
+	std::cout << RaiseRecursiveHalf(2, 0) << "\n";
+	std::cout << RaiseRecursiveHalf(3, 3) << "\n";
+	std::cout << RaiseRecursiveHalf(2, 6) << "\n";
+	printf("%d", R(3, 20));
 }
